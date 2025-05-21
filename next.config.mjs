214433bd1f6af75d1de9ff4468ad6bbc05e7efb7
iansilva2305 <armando.ita.silva@gmail.com>
2025-05-21 02:00:52 +0200

    Corregir configuración de despliegue para GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -21,7 +21,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        os: false,
+      };
+    }
     return config;
   },
 };
